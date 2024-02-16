9033883769c5e7e01fe7d709a556e32c833efbee
ibrittez <ignacio_brittez@outlook.com>
2024-02-16 20:57:25 -0300

    Dictionary sorter implementation.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 def main():
     return 0
 
+
+def get_sorted_letter_count_list(doc_letters):
+    sorted_list = []
+    for key in doc_letters:
+        sorted_list.append({"char": key, "count": doc_letters[key]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
 def get_document_letters_dict(document):
     text_content = {}
     for word in document.split():
