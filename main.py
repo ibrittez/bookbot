c7b4fd64a0e437daa4cb634ec3bee2764119a8cb
ibrittez <ignacio_brittez@outlook.com>
2024-02-16 20:55:44 -0300

    count_words function added.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     return 0
 
+def count_words(book):
+    words = book.split()
+    return len(words)
+
+
 def get_document_content(path):
     with open(path) as f:
         file_contents = f.read()
