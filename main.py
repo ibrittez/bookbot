09ac4697103e2df6bfe36dc9cf0081e2170cb8a2
ibrittez <ignacio_brittez@outlook.com>
2024-02-16 20:56:26 -0300

    get_word_letters_dict function added.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def main():
     return 0
 
+def get_word_letters_dict(word):
+    word_content = {}
+    lowered_word = word.lower()
+    for char in lowered_word:
+        if char in word_content:
+            word_content[char] += 1
+        else:
+            word_content[char] = 1
+    return word_content
+
+
 def count_words(book):
     words = book.split()
     return len(words)
